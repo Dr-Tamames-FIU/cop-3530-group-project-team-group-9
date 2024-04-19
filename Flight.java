a98b5fe8a632432abbd8f58da0ccb5d9728303c4
egarc258 <egarc258@gmail.com>
2024-04-19 19:45:41 -0400

    created flight class

---
+++ Flight.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+public class Flight {
+    // Flight properties
+    private String flightNumber;
+    private String departureAirport;
+    private String destinationAirport;
+    private Date departureTime;
+    private Date arrivalTime;
+
+    // Constructor
+    public Flight(String flightNumber, String departureAirport, String destinationAirport, Date departureTime, Date arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departureAirport = departureAirport;
+        this.destinationAirport = destinationAirport;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    // Getters and stters for flight properties
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
